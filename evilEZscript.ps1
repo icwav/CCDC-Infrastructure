b8740d1cbbc91c1c0a31f1d97be848f03e6a5491
TonightsBigLoser <ivancallejas96@gmail.com>
2023-03-02 05:50:19 -0800

    minor updates to evilEZscript

--- evilEZscript.ps1
+++ evilEZscript.ps1
@@ -1,6 +1,7 @@
+##
 
-
-
+#creating bad audit rules
+Write-Host "Creating bad audits..."  
     auditpol /set /category:"Account Logon" /success:disable 
     auditpol /set /category:"Account Logon" /failure:disable
     auditpol /set /category:"Account Management" /success:disable
@@ -20,11 +21,24 @@
     auditpol /set /category:"System" /success:disable 
     auditpol /set /category:"System" /failure:disable
 
+
+#Group Policy
+
+Write-Host "Creating Poor Group Polcies"
+Set-PolicyFileEntry -Path $env:systemroot\system32\GroupPolicy\Machine\registry.pol -Key "SOFTWARE\Policies\Microsoft\Messenger\Client" -ValueName PreventAutoRun -Type DWord -Data 0
+Set-PolicyFileEntry -Path $env:systemroot\system32\GroupPolicy\Machine\registry.pol -Key "SOFTWARE\Policies\Microsoft\SearchCompanion" -ValueName DisableContentFileUpdates -Type DWord -Data 0
+Set-PolicyFileEntry -Path $env:systemroot\system32\GroupPolicy\Machine\registry.pol -Key "SOFTWARE\Policies\Microsoft\Windows NT\IIS" -ValueName PreventIISInstall -Type DWord -Data 0
+Set-PolicyFileEntry -Path $env:systemroot\system32\GroupPolicy\Machine\registry.pol -Key "SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -ValueName NoAutoUpdate -Type DWord -Data 1
+
+
+#enabling services like SMB1 and telnet
+Write-Host "Enabling SMBv1 and Telnet..."
     dism /online /enable-feature /featurename:TelnetClient
     dism /online /enable-feature /featurename:TelnetServer
     dism /online /enable-feature /featurename:"SMB1Protocol"
     Set-SmbServerConfiguration -EnableSMB1Protocol $true -Force
 
+Write-Host "Reducing Known Attack Surfaces..."
     #bad WMI
     Set-MpPreference -PUAProtection disable
     #bad smb protection
@@ -34,7 +48,7 @@
     #LSASS bad config
     Add-MpPreference -AttackSurfaceReductionRules_Ids 9E6C4E1F-7D60-472F-BA1A-A39EF669E4B2 -AttackSurfaceReductionRules_Actions Disabled
     
-
+Write-Host "Editing Registry Keys..."
     #bad windows defender registry edits
     reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d 1 /f
     reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v "ServiceKeepAlive" /t REG_DWORD /d 0 /f
@@ -42,14 +56,11 @@
     reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRealtimeMonitoring" /t REG_DWORD /d 1 /f
     reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Scan" /v "CheckForSignaturesBeforeRunningScan" /t REG_DWORD /d 0 /f
     reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Scan" /v "DisableHeuristics" /t REG_DWORD /d 1 /f
-
-    reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\LanmanWorkstation" /v AllowInsecureGuestAuth /t REG_DWORD /d 0 /f
-
+    reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\LanmanWorkstation" /v AllowInsecureGuestAuth /t REG_DWORD /d 1 /f
     reg add "HKLM\SYSTEM\CurrentControlSet\services\LanmanWorkstation\Parameters" /v EnablePlainTextPassword /t REG_DWORD /d 1 /f
 
     #enables storage of domain passwords
     reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v disabledomaincreds /t REG_DWORD /d 0 /f
-
     reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymous /t REG_DWORD /d 0 /f
     reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymoussam /t REG_DWORD /d 0 /f
     #doesnt limit blank passwords
@@ -63,3 +74,21 @@
     #disable UAC
     reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
     
+#start and enable Home Group services
+Write-Host "Starting and enabling Home Group Services..."
+Start-Service "HomeGroupListener"
+Set-Service "HomeGroupListener" - StartupType Enabled
+Start-Service "HomeGroupProvider"
+Set-Service "HomeGroupProvider" -StartupType Enabled
+
+Write-Host "Enabling Remote Desktop..."
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Type DWord -Value 0
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name "UserAuthentication" -Type DWord -Value 0
+
+#Enable Autorun
+Write-Host "Enabling Autorun..."
+Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoDriveTypeAutoRun"
+
+#Hide hidden files
+Write-Host "Hiding Hidden Files..."
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Hidden" -Type DWord -Value 2
